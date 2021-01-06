997ca70ae83fcba1a11fb6706a34d692f03e5cf0
João Knoller <joaofelipeknollermarques@gmail.com>
2021-01-06 03:06:07 -0300

    update final

--- projeto-onu/js/script.js
+++ projeto-onu/js/script.js
@@ -1,3 +1,11 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+document.querySelector(".abas").addEventListener("click", () =>
+    document.querySelector(".sections").classList.toggle("show-notices")
+);
+
+function contactButtonFunction() {
+    document.querySelector(".hamburguer").click();
+}
